dc6f67f040aa22d69ca6d04db0464d6212b78b29
Nichole O'Neill <nazm612@gmail.com>
2024-02-12 21:37:57 -0600

    created front-end react app and back-end Spring app

---
+++ back_end/src/main/java/com/project/plantMe/PlantMeApplication.java
@@ -0,0 +1,13 @@
+package com.project.plantMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantMeApplication.class, args);
+	}
+
+}
